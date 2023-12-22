3a61666a1193f3a597197686d6c6d5218486b382
Urvish Patel <urvish.xyz@gmail.com>
2023-12-22 23:34:43 +0530

    all basic files added

---
+++ script.js
@@ -0,0 +1,16 @@
+function nextPage() {
+    window.location.href = "yes.html";
+}
+
+function moveButton() {
+    var button = document.getElementById('noButton');
+    var x = Math.random() * (window.innerWidth - button.offsetWidth);
+    var y = Math.random() * (window.innerHeight - button.offsetHeight);
+
+    // Ensure the button stays within the viewport boundaries
+    x = Math.max(0, Math.min(x, window.innerWidth - button.offsetWidth));
+    y = Math.max(0, Math.min(y, window.innerHeight - button.offsetHeight));
+
+    button.style.left = `${x}px`;
+    button.style.top = `${y}px`;
+}
