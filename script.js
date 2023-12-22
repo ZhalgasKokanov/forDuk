a468f1e14eb48b3a0e64a4624739c01ea691acec
Urvish Patel <urvish.xyz@gmail.com>
2023-12-23 01:27:29 +0530

    added functionality of ask Name and display

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ function moveButton() {
 }
 
 console.log("Made with love by - Urvish Patel ❣️");
+
+
+function askName() {
+    var userName;
+
+    // Keep prompting until a valid name is provided
+    while (!userName) {
+        userName = prompt("What is your name?");
+    }
+
+    // Store the user's name in localStorage for later use
+    localStorage.setItem('userName', userName);
+}
