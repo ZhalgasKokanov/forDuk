9d3341d4aeacd5cc497f6c094c902d1f6316536d
Urvish Patel <urvish.xyz@gmail.com>
2023-12-23 17:53:51 +0530

    space fixed in askname

--- script.js
+++ script.js
@@ -12,12 +12,25 @@ function moveButton() {
 console.log("Made with love by - Urvish Patel ❣️");
 
 
+// function askName() {
+//     var userName;
+
+//     // Keep prompting until a valid name is provided
+//     while (!userName) {
+//         userName = prompt("What is your name?");
+//     }
+
+//     // Store the user's name in localStorage for later use
+//     localStorage.setItem('userName', userName);
+// }
+
+// for space 
 function askName() {
     var userName;
 
     // Keep prompting until a valid name is provided
-    while (!userName) {
-        userName = prompt("What is your name?");
+    while (!userName || /\s/.test(userName)) {
+        userName = prompt("What is your name? Please enter a name without spaces.");
     }
 
     // Store the user's name in localStorage for later use
